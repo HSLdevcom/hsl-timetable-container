db3bee1e33009b167223d9289b7293b0fa7698e9
TeemuP <teemu.peltonen@cgi.com>
2018-07-12 10:02:16 +0300

    Travis build & slack username

--- container-scripts/timetableDataBuilder.sh
+++ container-scripts/timetableDataBuilder.sh
@@ -4,7 +4,7 @@ set -e
 echo "HSL timetable data build started"
 if [ -v SLACK_WEBHOOK_URL ]; then
     curl -X POST -H 'Content-type: application/json' \
-         --data '{"text":"HSL timetable data build started\n"}' $SLACK_WEBHOOK_URL
+         --data '{"username":"HSL timetable builder","text":"HSL timetable data build started\n"}' $SLACK_WEBHOOK_URL
 fi
 
 
@@ -12,14 +12,14 @@ fi
 if [ $? -eq 0 ]; then
     echo "HSL timetable data build finished"
     if [ -v SLACK_WEBHOOK_URL ]; then
-            { echo -e "HSL timetable data build finished:\n..."; tail -n 1 /tmp/generate.log; } | jq -R -s '{text: .}' | \
+            { echo -e "HSL timetable data build finished:\n..."; tail -n 1 /tmp/generate.log; } | jq -R -s '{text: .,username:"HSL timetable builder"}' | \
             curl -X POST -H 'Content-type: application/json' -d@- $SLACK_WEBHOOK_URL
     fi
 else
         echo "HSL timetable data build failed"
     if [ -v SLACK_WEBHOOK_URL ]; then
         #extract log end which most likely contains info about failure
-        { echo -e "HSL timetable data build failed:\n..."; tail -n 40 /cronlogs/cron.log; } | jq -R -s '{text: .}' | \
+        { echo -e "HSL timetable data build failed:\n..."; tail -n 40 /cronlogs/cron.log; } | jq -R -s '{text: .,username:"HSL timetable builder"}' | \
             curl -X POST -H 'Content-type: application/json' -d@- $SLACK_WEBHOOK_URL
     fi
 fi
