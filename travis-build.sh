bed856ff209f85684fd2fb13551702a31cca8bda
Ristomatti Karppanen <ristomatti.karppanen@cgi.com>
2020-10-29 07:28:52 +0200

    Updated Docker tagging

--- travis-build.sh
+++ travis-build.sh
@@ -2,15 +2,20 @@
 set -e
 
 ORG=${ORG:-hsldevcom}
-DOCKER_TAG=${TRAVIS_BUILD_NUMBER:-latest}
-DOCKER_IMAGE=$ORG/hsl-timetable-builder:${DOCKER_TAG}
-DOCKER_IMAGE_LATEST=$ORG/hsl-timetable-builder:latest
+DOCKER_IMAGE=$ORG/hsl-timetable-builder
+DOCKER_TAG="latest"
 
-docker build --tag=$DOCKER_IMAGE .
+DOCKER_TAG_LONG=$DOCKER_TAG-$(date +"%Y-%m-%dT%H.%M.%S")-${TRAVIS_COMMIT:0:7}
+DOCKER_IMAGE_LATEST=$DOCKER_IMAGE:latest
+DOCKER_IMAGE_TAG=$DOCKER_IMAGE:$DOCKER_TAG
+DOCKER_IMAGE_TAG_LONG=$DOCKER_IMAGE:$DOCKER_TAG_LONG
+
+
+docker build --tag=$DOCKER_IMAGE_TAG_LONG .
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
   docker login -u $DOCKER_USER -p $DOCKER_AUTH
-  docker push $DOCKER_IMAGE
-  docker tag $DOCKER_IMAGE $DOCKER_IMAGE_LATEST
+  docker push $DOCKER_IMAGE_TAG_LONG
+  docker tag $DOCKER_IMAGE_TAG_LONG $DOCKER_IMAGE_LATEST
   docker push $DOCKER_IMAGE_LATEST
 fi
