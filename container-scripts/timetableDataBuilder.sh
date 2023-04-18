4b2288e3f210e68169db626c95ac1c71477896d8
Vesa Meskanen <vesa.meskanen@cgi.com>
2023-04-18 15:10:55 +0300

    Add quotes around parameter expansion

--- container-scripts/timetableDataBuilder.sh
+++ container-scripts/timetableDataBuilder.sh
@@ -24,20 +24,20 @@ if [ $SUCCESS -eq 0 ]; then
     echo "$SLACKUSER finished"
     if [ -n "${SLACK_CHANNEL_ID}" ]; then
         MSG=$({ tail -n 1 /tmp/generate.log && tail -n 1 /tmp/fetch.log; } | jq -R -s '{"channel": "'$SLACK_CHANNEL_ID'", "username": "'$SLACKUSER'", "thread_ts": "'$TIMESTAMP'", "text": .}')
-        slackpost $MSG $SLACKPOSTURL
+        slackpost "$MSG" "$SLACKPOSTURL"
 
 	MSG='{"channel": "'$SLACK_CHANNEL_ID'","text": "'$SLACKUSER' finished :white_check_mark:", "username": "'$SLACKUSER'", "ts": "'$TIMESTAMP'"}';
-	slackpost $MSG $SLACKUPDATEURL
+	slackpost "$MSG" "$SLACKUPDATEURL"
     fi
 else
     echo "$SLACKUSER failed"
     if [ -n "${SLACK_CHANNEL_ID}" ]; then
         #extract log end which most likely contains info about failure
         MSG=$({ echo -e "Dataloading log: \n"; tail -n 40 /cronlogs/cron.log; } | jq -R -s '{"channel": "'$SLACK_CHANNEL_ID'", "username": "'$SLACKUSER'", "thread_ts": "'$TIMESTAMP'", "text": .}')
-	slackpost $MSG $SLACKPOSTURL
+	slackpost "$MSG" "$SLACKPOSTURL"
 
         MSG='{"channel": "'$SLACK_CHANNEL_ID'", "text": "'$SLACKUSER' failed :boom:", "username": "'$SLACKUSER'", "ts": "'$TIMESTAMP'"}'
-	slackpost $MSG $SLACKUPDATEURL
+	slackpost "$MSG" "$SLACKUPDATEURL"
     fi
 fi
 
